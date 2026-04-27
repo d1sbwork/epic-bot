64d2555de3acbd19556bd2740c5ba9f88770909e
disba <disba@gmail.com>
2026-04-27 13:52:04 +0500

    add routes

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Backend работает 🚀"}
-
 
 @app.get("/balance/{user_id}")
 async def get_balance(user_id: int):
